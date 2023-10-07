f339cb7f5f4bf7ef4da470896e2ed805475a2c0a
mohsen zamani <9636012+mohsenzamani@users.noreply.github.com>
2023-10-07 15:55:00 +0330

    create spring boot project

---
+++ src/main/java/ir/programmingblog/arisasocialnetwork/ArisaSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package ir.programmingblog.arisasocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArisaSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArisaSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/programmingblog/arisasocialnetwork/ArisaSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.programmingblog.arisasocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArisaSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
